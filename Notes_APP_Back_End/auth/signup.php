9f212fc9ba5c21bcc8120a02238c4a9719b0114c
Shadi Naffaa <shadynaf11@gmail.com>
2024-10-31 13:03:16 +0400

    Add files via upload

---
+++ Notes_APP_Back_End/auth/signup.php
@@ -0,0 +1,15 @@
+<?php
+include "../connect.php";
+
+$username   =  filterRequest("username");
+$email      =  filterRequest("email");
+$password   =  filterRequest("password");
+
+$stmt = $con->prepare("INSERT INTO `users`(`username`, `email`, `password`) VALUES (?,?,?)");
+$stmt->execute(array($username, $email, $password));
+$count = $stmt->rowCount();
+if ($count > 0) {
+    echo json_encode(array("status" => "Success"));
+} else {
+    echo json_encode(array("status" => "Fail"));
+}
